9437fa6433492974a08c4b515b637822de589444
ihasidul <ihasidul@gmail.com>
2020-05-13 16:10:17 +0600

    Added new user files

---
+++ app/controllers/UserController.php
@@ -0,0 +1,12 @@
+
+<?php
+class UserController extends Controller
+{
+    public function index()
+    {
+        // Return a 'view' or do nothing.
+        $this->view('UserView');
+    }
+}
+
+?>
---
+++ app/views/UserView.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="css/user.css">
+<title>User</title>
+
+<body>
+    <ul>
+        <li><a class="active">Content Rating Site</a></li>
+        <li><input class="search" type="search" placeholder="Search.." name="search"> </input></li>
+        <li><button id="icon" type="submit"><i class="fa fa-search"></i></button></li>
+
+
+
+        <div class="dropdown" style="float:right;">
+
+            <button style="width:250px;" class="dropbtn">USER</button>
+
+            <div class="dropdown-content">
+                <a href="#" id="rcorners1">Watch List</a><br>
+                <a href="#" id="rcorners1">Profile</a><br>
+                <a href="#" id="rcorners1">Log Out</a><br>
+            </div>
+        </div>
+
+    </ul>
+
+    <table>
+
+
+        <tr>
+            <th>
+
+
+                <div style="font-size:20px;" id="rcorners3">
+                    <form>
+
+                        <h2> New Moviess</h2>
+
+                        <MARQUEE SCROLLDELAY=50>
+                            <a href="http://localhost/ndorai.php"> <img src="8.JFIF"> </a>
+                            <a href="http://localhost/jdiekdin.php"> <img src="r1.png"></a>
+                            <a href="http://localhost/aynabaji.php"> <img src="1.jpg"> </a>
+                            <a href="http://localhost/rate.php"> <img src="2.jpg"> </a>
+                            <a href="http://localhost/television.php"> <img src="3.jpg"> </a>
+                        </marquee>
+                        <br>
+                        <h2> New Natoks</h2>
+                        <marquee behavior="ALTERNATE" direction="left">
+                            <a href="http://localhost/bach.php"><img src="11.jpg"> </a>
+                            <a href="http://localhost/vhalobasha101.php"> <img src="33.jpg"> </a>
+                            <a href="http://localhost/bokhate.php"><img src="10.jfif"> </a>
+                            <a href="http://localhost/lol.php"><img src="101.jpg"></a>
+                        </marquee>
+                        <br>
+
+                        <h2>Comedy Natoks</h2>
+                        <img src="c1.jpg">
+                        <img src="11.jpg">
+                        <img src="103.png">
+                        <img src="c5.JFIF">
+                        <br>
+                        <h2>Action Movies & Crime </h2>
+                        <a href="http://localhost/rate.php"> <img src="2.jpg"> </a>
+                        <img src="a2.png">
+                        <img src="r4.jpg ">
+                        <img src="war2.JFIF">
+                        <br>
+
+                        <h2>Romamtic Movies </h2>
+                        <img src="ro3.jpg">
+                        <img src="ro2.jpg">
+                        <img src="ro4.jpg ">
+                        <img src="ro1.JPG">
+                        <br>
+
+
+
+
+                        <h2>War Movies</h2>
+                        <img src="war1.jpg">
+                        <img src="war2.JFIF">
+                        <img src="war3.jpg">
+                        <img src="war4.JPG">
+
+                        <br>
+
+
+
+
+            </th>
+        </tr>
+        </form>
+        </div>
+        </div>
+        </td>
+        </tr>
+    </table>
+
+</body>
+
+</html>
