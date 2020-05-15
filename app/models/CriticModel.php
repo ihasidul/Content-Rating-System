fa5cb2d6ee4eb349b7f3dbc975753707a63f4288
ihasidul <ihasidul@gmail.com>
2020-05-15 12:32:31 +0600

    all info getting function added

--- app/models/CriticModel.php
+++ app/models/CriticModel.php
@@ -24,4 +24,23 @@ class CriticModel
 
         return $result->num_rows;
     }
+    public function getAllCritic()
+    {
+        $sql = "SELECT * FROM critics";
+        $db = new DataAccess();
+
+        $result = $db->getData($sql);
+        if (mysqli_num_rows($result) > 0) {
+            // output data of each row
+            while ($row = mysqli_fetch_assoc($result)) {
+                $users[] =  array(
+                    "User ID" => $row["id"],
+                    "Name" => $row["name"], "Email" => $row["email"], "Phone" => $row["phone"]
+                );
+            }
+            return $users;
+        } else {
+            echo "0 results";
+        }
+    }
 }
