6e238b2cfbc3b10db2996885045b5a9201e34ceb
ihasidul <ihasidul@gmail.com>
2020-05-15 12:32:22 +0600

    all info getting function added

--- app/models/ContentCreatorModel.php
+++ app/models/ContentCreatorModel.php
@@ -23,4 +23,26 @@ class ContentCreatorModel
 
         return $result->num_rows;
     }
+
+    public function getAllContentCreator()
+    {
+        $sql = "SELECT * FROM contentcreator";
+        $db = new DataAccess();
+
+        $result = $db->getData($sql);
+        if (mysqli_num_rows($result) > 0) {
+            // output data of each row
+            while ($row = mysqli_fetch_assoc($result)) {
+                $users[] =  array(
+                    "User ID" => $row["id"],
+                    "Name" => $row["name"],
+                    "Email" => $row["email"],
+                    "Phone" => $row["phone"]
+                );
+            }
+            return $users;
+        } else {
+            echo "0 results";
+        }
+    }
 }
