2bc520310ff5496a33c5d508859c6b0d918db0b6
ihasidul <ihasidul@gmail.com>
2020-05-16 19:32:42 +0600

    New db with more data

--- crsdb.sql
+++ crsdb.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 16, 2020 at 12:55 PM
+-- Generation Time: May 16, 2020 at 03:18 PM
 -- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.3
+-- PHP Version: 7.4.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -66,7 +66,30 @@ CREATE TABLE `content` (
 --
 
 INSERT INTO `content` (`id`, `content_creator`, `name`, `type`, `genre`, `posterName`, `cast`, `date`, `ratting`, `criticratting`) VALUES
-('conent-1', 'cc-123', 'Aynabaji', 'movie', 'thriller', 'aynabaji.jpg', 'castinfo', '2020-05-13', 8, 7.8);
+('conent-1', 'cc-123', 'Aynabaji', 'movie', 'thriller', 'aynabaji.jpg', 'castinfo', '2020-05-13', 8, 7.8),
+('Content-10', 'cc-9', 'family crisis', 'natok', 'Drama', 'family crisis.png', 'castinfo', '2019-05-13', 6.3, 6.9),
+('Content-11', 'cc-9', 'bokhate', 'natok', 'Action', 'bokhate.jfif', 'castinfo', '2018-08-13', 7.3, 7),
+('Content-12', 'cc-9', 'mission barishal ', 'natok', 'Comedy', 'mission barishal.png', 'castinfo', '2019-05-13', 6.3, 6),
+('Content-13', 'cc-7', 'Cinemaal', 'Video content', 'Comedy', 'Cinemaal.jpg', 'castinfo', '2012-05-13', 6.3, 6),
+('Content-14', 'cc-9', 'Extraction Bangla Funny Dubbing', 'Video content', 'Comedy', 'Extraction Bangla Funny Dubbing.jpg', 'castinfo', '2014-06-18', 6.3, 7.9),
+('Content-15', 'cc-9', 'DESHI AVENGERS', 'Video content', 'Comedy', 'DESHI AVENGERS.jpg', 'castinfo', '2019-08-13', 7.3, 6.3),
+('Content-16', 'cc-9', 'THE THEORY OF \'COPY FILM\'', 'Video content', 'Comedy', 'THE THEORY OF \'COPY FILM\'.jpg', 'castinfo', '2019-05-13', 5.3, 5.9),
+('Content-17', 'cc-9', 'The 3 StoogesBangla Funny Dubbing', 'Video content', 'Comedy', 'The 3 StoogesBangla Funny Dubbing.jpg', 'castinfo', '2014-06-18', 6.3, 6.9),
+('Content-18', 'cc-9', 'HSC Exam Special Funny Dubbing ', 'Video content', 'Comedy', 'HSC Exam Special Funny Dubbing.jpg', 'castinfo', '2019-05-19', 8.3, 8),
+('Content-19', 'cc-7', 'dark', 'Tv series', 'Drama', 'dark.jpg', 'castinfo', '2017-05-13', 8.7, 9),
+('Content-2', 'cc-7', 'Dhaka Attack', 'movie', ' Action/Thriller', 'Dhaka Attack.jpg', 'castinfo', '2017-05-13', 7.9, 8),
+('Content-20', 'cc-7', 'stranger things', 'Tv series', 'Drama', 'stranger things.jpg', 'castinfo', '2017-05-13', 7.3, 7.9),
+('Content-21', 'cc-7', 'peaky blinders', 'Tv series', 'Drama', 'peaky blinders.jpg', 'castinfo', '2013-05-13', 8.8, 8.9),
+('Content-22', 'cc-7', 'Money Heist', 'Tv series', 'Drama', 'Money Heist.jpg', 'castinfo', '2018-06-26', 8.4, 8.6),
+('Content-23', 'cc-7', 'the flash', 'Tv series', 'Drama', 'the flash.jpg', 'castinfo', '2014-05-13', 7.8, 8.1),
+('Content-24', 'cc-7', 'breaking bad', 'Tv series', 'Drama', 'breaking bad.jpg', 'castinfo', '2008-06-18', 9.5, 9.8),
+('Content-3', 'cc-7', 'Television', 'movie', 'Drama', 'Television.jpg', 'castinfo', '2012-05-13', 8.2, 7.9),
+('Content-4', 'cc-7', 'PoraMon 2', 'movie', 'Romance', 'PoraMon 2.jpg', 'castinfo', '2018-06-13', 6.7, 8),
+('Content-5', 'cc-7', 'Dahan', 'movie', 'Drama', 'Dahan.jpg', 'castinfo', '2018-08-13', 7.6, 7.9),
+('Content-6', 'cc-7', 'Fagun Haway', 'movie', 'Drama', 'Fagun Haway.jpg', 'castinfo', '2019-05-13', 8.6, 8),
+('Content-7', 'cc-9', 'valobasha 101', 'natok', 'Romance', 'valobasha 101.jpg', 'castinfo', '2014-06-18', 7, 7.2),
+('Content-8', 'cc-9', 'lol', 'natok', 'Romance', 'lol.jpg', 'castinfo', '2019-05-13', 6.3, 6.5),
+('Content-9', 'cc-9', 'bachelor point', 'natok', 'Comedy', 'bachelor point.jpg', 'castinfo', '2018-06-13', 8.9, 8.6);
 
 -- --------------------------------------------------------
 
