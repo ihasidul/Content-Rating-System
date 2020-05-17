ed14d25c535cf9cccf38aa26d47da8c9a2423bb6
ihasidul <ihasidul@gmail.com>
2020-05-17 18:14:49 +0600

    added user rating function

--- app/controllers/ContentController.php
+++ app/controllers/ContentController.php
@@ -18,6 +18,7 @@ class ContentController extends Controller
         $contentObj = $this->model('ContentModel');
         $content = $contentObj->getContentInfoById((int) $content_id);
         $comments = $contentObj->getComments((int) $content_id);
+        $movies = $contentObj->getAllMovies();
         //echo $content_id;
         //echo var_dump($content_id);
         //new
@@ -28,10 +29,25 @@ class ContentController extends Controller
             "Id" => $_SESSION['id'],
             "Password" => $_SESSION['password'],
             "Content" => $content,
-            "Comments" => $comments
+            "Comments" => $comments,
+            "Movies" => $movies,
+            "ContentID" => $content_id
         ];
 
 
         $this->view("ContentView", $data);
+    }
+
+
+
+    public function submitUserRatingAndComment()
+    {
+        echo $_POST["comment"];
+        echo $_POST["rating"];
+        echo $_POST["content_id"];
+        echo $_SESSION["id"];
+        $contentObj = $this->model('ContentModel');
+        $contentObj->updateCommentAndRating($_POST["content_id"], $_SESSION["id"], $_POST["comment"], $_POST["rating"]);
+        header("Location: content/" . $_POST["content_id"]);
     }
 }
