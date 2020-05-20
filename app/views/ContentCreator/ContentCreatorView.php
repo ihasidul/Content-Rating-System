008bc0d239df6b4ca40631233399d5a1580206f0
ihasidul <ihasidul@gmail.com>
2020-05-20 15:17:45 +0600

    fixed the table error

--- app/views/ContentCreator/ContentCreatorView.php
+++ app/views/ContentCreator/ContentCreatorView.php
@@ -1,6 +1,7 @@
 <?php
 $ContentList = $data["ContentsOfThisCC"];
 // var_dump($ContentList);
+// var_dump(json_encode($ContentList));
 ?>
 <!DOCTYPE html>
 <html>
@@ -8,7 +9,7 @@ $ContentList = $data["ContentsOfThisCC"];
 <head>
     <meta http-equiv="Content-type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>Admin Dashboard</title>
+    <title>Content Creator Dashboard</title>
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4-4.1.1/dt-1.10.21/sp-1.1.0/datatables.min.css" />
 
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
@@ -173,7 +174,7 @@ $ContentList = $data["ContentsOfThisCC"];
                 "data": "Date"
             },
             {
-                "data": "User Ratting"
+                "data": "User Rating"
             },
             {
                 "data": "Critic Rating"
